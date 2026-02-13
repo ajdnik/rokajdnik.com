1302de006db9fec971c3b395025e4687b59a1059
Rok Ajdnik <r.ajdnik@gmail.com>
2026-02-13 09:58:11 +0100

    feat: refactor date formatting

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,9 @@
+export function formatDate(date: Date): string {
+  return date
+    .toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    })
+    .slice(0, 11);
+}
